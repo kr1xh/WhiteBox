884e41252f30eaf78a690536c39acabdc3d0da37
Krish <krishchandpurkar20@gmail.com>
2026-05-17 00:13:24 +0530

    ringbuffer: add javascript solution - Whitebox→GitHub

--- ringbuffer/ringbuffer.js
+++ ringbuffer/ringbuffer.js
@@ -1,7 +1,9 @@
 // Daily Quests
 // Difficulty: Unknown
 // Category: Done
-// Topics: Done, Auto, Mark solved, Claim, C++ 17, Submitting..., QUESTS, Resets in 14h 16m, 30 / 240 XP · 50 / 400 coins, 0/1, Python3
+// Topics: Done, Auto, Mark solved, Claim, Runtime, Memory, Language, Tests, C++ 17, INPUT:, EXPECTED:, OUTPUT:, QUESTS, Resets in 14h 16m, 30 / 240 XP · 50 / 400 coins, 0/1, Accepted, Beats 88% of C++ 17 submissions, 3.0ms, 944 KB
+// Time: 4 ms
+// Space: 1.1 MB
 // Source: https://whitebox.ac/problems/ringbuffer
 // Pushed by Whitebox→GitHub Extension
 capacity = cap;
