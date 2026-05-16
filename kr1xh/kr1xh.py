cc852a6855300b2abdb5ff5da88c02974362da2c
Krish <krishchandpurkar20@gmail.com>
2026-05-16 23:52:22 +0530

    kr1xh: Time: N/A, Space: N/A - Whitebox→GitHub

---
+++ kr1xh/kr1xh.py
@@ -0,0 +1,7 @@
+# kr1xh
+# Difficulty: Easy 1
+# Category: BasicsData Structures
+# Topics: BasicsData Structures, Basics, Data Structures, Done, Auto, Mark solved, Claim, POWER LEVEL, XP, RANK, SOLVED, ACCURACY, BEST STREAK, QUIZ SOLVED, QUIZ ACCURACY, PROB SOLVED, char II → char III, May, Sat, Jan, Feb, Mar, Apr, Jun, Jul, Aug, Sep, Oct, Nov, Dec, M  W  F, QUESTS, Resets in 14h 37m, 30 / 240 XP · 50 / 400 coins, 0/1, NEW, SOON, 3,523, White Room SOON, char II RANK, char II, char III, +30 today
+# Source: https://whitebox.ac/profile/kr1xh
+# Pushed by Whitebox→GitHub Extension
+# Paste your solution here
