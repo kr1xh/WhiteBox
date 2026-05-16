160a19b1034c83cc6dd625b901b3408636f0e5f0
Krish <krishchandpurkar20@gmail.com>
2026-05-16 23:53:28 +0530

    welcome-back: Time: N/A, Space: N/A - Whitebox→GitHub

---
+++ welcome-back/welcome-back.py
@@ -0,0 +1,7 @@
+# Welcome back
+# Difficulty: Unknown
+# Category: NEW
+# Topics: NEW, SOON, White Room SOON
+# Source: https://whitebox.ac/sign-in
+# Pushed by Whitebox→GitHub Extension
+# Paste your solution here
