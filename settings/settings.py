e9d797b9de3c0a86d6eb5c6e44c1f8cc1566d695
Krish <krishchandpurkar20@gmail.com>
2026-05-16 23:51:57 +0530

    settings: Time: N/A, Space: N/A - Whitebox→GitHub

--- settings/settings.py
+++ settings/settings.py
@@ -1,7 +1,7 @@
 # Settings
 # Difficulty: Unknown
 # Category: Done
-# Topics: Done, Auto, Mark solved, Claim, QUESTS, Resets in 14h 39m, 30 / 240 XP · 50 / 400 coins, 0/1, NEW, SOON, 3,473, White Room SOON
+# Topics: Done, Auto, Mark solved, Claim, Goal, Experience Level: University Student, DSA Level: Complete Noob, Target Timeline, QUESTS, Resets in 14h 38m, 30 / 240 XP · 50 / 400 coins, 0/1, NEW, SOON, 3,523, White Room SOON
 # Source: https://whitebox.ac/settings
 # Pushed by Whitebox→GitHub Extension
 # Paste your solution here
