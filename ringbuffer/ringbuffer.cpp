4d397e5c8af98ccda50600a048e962d7c5f018ed
Krish <krishchandpurkar20@gmail.com>
2026-05-17 15:03:50 +0530

    ringbuffer: add C++ 17 solution - Whitebox→GitHub

--- ringbuffer/ringbuffer.cpp
+++ ringbuffer/ringbuffer.cpp
@@ -1,7 +1,9 @@
-# Put a ring on it.
-# Difficulty: Easy 1
-# Category: Basics
-# Topics: Basics, Data Structures, Claim, Auto, Example 1, Input:, Output:, Accepted, Acceptance, Next up, C++ 17, Executing..., QUESTS, Resets in 23h 26m, 0 / 225 XP · 0 / 400 coins, 0/5, 0/1, 10s, 256 MB, Jan 1, 2024, Google
+# Daily Quests
+# Difficulty: Unknown
+# Category: Claim
+# Topics: Claim, Auto, Runtime, Memory, Language, Tests, C++ 17, INPUT:, EXPECTED:, OUTPUT:, QUESTS, Resets in 23h 26m, 0 / 225 XP · 0 / 400 coins, 0/5, 0/1, Accepted, Beats 89% of C++ 17 submissions, 3.0ms, 996 KB
+# Time: 4 ms
+# Space: 1.2 MB
 # Source: https://whitebox.ac/problems/ringbuffer
 # Pushed by Whitebox→GitHub Extension
 #include <iostream>
