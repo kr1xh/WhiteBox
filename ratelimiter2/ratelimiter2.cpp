065b61ccd8f3744be579571736796b88272f69fc
Krish <krishchandpurkar20@gmail.com>
2026-05-20 02:16:56 +0530

    ratelimiter2: add C++ 17 solution - Whitebox→GitHub

---
+++ ratelimiter2/ratelimiter2.cpp
@@ -0,0 +1,16 @@
+# Know your limits. (Hard version)
+# Difficulty: Unknown
+# Category: Basics
+# Topics: Basics, Data Structures, Networking
+# Source: https://whitebox.ac/problems/ratelimiter2
+# Pushed by Whitebox→GitHub Extension
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+    return 0;
+}
