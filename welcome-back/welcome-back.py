ac69b33f405fadacf714bf15157243ed22fce5df
Krish <krishchandpurkar20@gmail.com>
2026-05-19 20:56:47 +0530

    Delete welcome-back directory

--- welcome-back/welcome-back.py
+++
@@ -1,7 +0,0 @@
-# Welcome back
-# Difficulty: Unknown
-# Category: NEW
-# Topics: NEW, SOON, White Room SOON
-# Source: https://whitebox.ac/sign-in
-# Pushed by Whitebox→GitHub Extension
-# Paste your solution here
