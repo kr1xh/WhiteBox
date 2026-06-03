8567c6b9220a82f10d8257f3004b1c1b7be5e149
Krish <krishchandpurkar20@gmail.com>
2026-06-03 15:08:23 +0530

    algorithm-roadmap: Time: N/A, Space: N/A - Whitebox→GitHub

---
+++ algorithm-roadmap/algorithm-roadmap.py
@@ -0,0 +1,7 @@
+# Algorithm Roadmap
+# Difficulty: Easy 1
+# Category: Noob 0
+# Topics: Noob 0, Normal 3, Normal+ 4, Insane 7, Expert 8, Master 9, BFS & DFS 0/17  ⊕, Complete Search 0/14  ⊕, Recursion 0/8  ⊕, Disjoint Union Set (DSU) 0/16  ⊕, Prefix Sums 0/14  ⊕, Stacks & Queues 0/7  ⊕, More Graph Algorithms 0/16  ⊕, LCA & Tree Queries 0/1  ⊕, Network Flow  ⊕, Getting Started 0/18  ⊕, Fenwick Tree (BIT) 0/3  ⊕, Range Queries  ⊕, Binary Search 0/13  ⊕, Trees & Binary Trees 0/11  ⊕, Segment Trees 0/3  ⊕, Advanced Graphs  ⊕, Stuff you'll probably never use  ⊕, DP Level 2 0/3  ⊕, DP Level 4 0/1  ⊕, DP Level 1 0/30  ⊕, DP Level 3  ⊕, Basic Strings 0/5  ⊕, Advanced Strings  ⊕, Math Level 1 0/13  ⊕, Math Level 2 0/10  ⊕, Math Level 3  ⊕, Computational Geometry  ⊕, Auto, Done, 1 Easy, 2 Easy+, 3 Normal, 4 Normal+, 5 Hard, 6 Harder, 7 Insane, 8 Expert, 9 Master, 10 Grandmaster, 11 Glitched, 12 Mythic, 13 Mythic+, 14 Legendary, QUESTS, Resets in 23h 21m, 60 / 330 XP · 100 / 550 coins, 0/1, NEW, SOON, 4,695, Maps & Sets 0/10, Two Pointers 0/6, Sliding Window 0/19, Heaps & Priority Queues 0/9, Tries 0/11, Sorting 0/9, Greedy 0/10, Bitwise Operations 0/12, Generating Functions, Ad-hoc 0/8, Constructive Algorithms 0/1, Interactive, Divide & Conquer
+# Source: https://whitebox.ac/algorithms
+# Pushed by Whitebox→GitHub Extension
+# Paste your solution here
