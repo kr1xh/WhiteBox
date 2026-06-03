be2a29fe5cdbeeaeeb87205b0d41557c364786cf
Krish <krishchandpurkar20@gmail.com>
2026-06-03 15:20:16 +0530

    judges-online-resources: Time: N/A, Space: N/A - Whitebox→GitHub

--- judges-online-resources/judges-online-resources.py
+++ judges-online-resources/judges-online-resources.py
@@ -1,7 +1,7 @@
 # Judges & Online Resources
 # Difficulty: Unknown
-# Category: guide
-# Topics: guide, paidcourse, paid, course, All types, book, judge, problem set, All tags, interview, cses, codeforces, reference, leetcode, atcoder, usaco, olympiad, dsa, Priority only, Auto, Claim, Mark solved, QUESTS, Resets in 23h 10m, 0 / 225 XP · 0 / 400 coins, 0/5, 0/1, NEW, SOON, 4,695
+# Category: dsainterview
+# Topics: dsainterview, dsa, interview, All types, book, course, guide, judge, problem set, All tags, cses, codeforces, reference, paid, leetcode, atcoder, usaco, olympiad, Priority only, Auto, Claim, Mark solved, QUESTS, Resets in 23h 09m, 0 / 225 XP · 0 / 400 coins, 0/5, 0/1, NEW, SOON, 4,695
 # Source: https://whitebox.ac/library/cp-judges
 # Pushed by Whitebox→GitHub Extension
 # Paste your solution here
