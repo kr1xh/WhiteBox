0ad8fef33b967285f411d7beed3ff505b9c93ffd
Krish <krishchandpurkar20@gmail.com>
2026-06-03 15:08:09 +0530

    domain-knowledge: Time: N/A, Space: N/A - Whitebox→GitHub

---
+++ domain-knowledge/domain-knowledge.py
@@ -0,0 +1,7 @@
+# Domain Knowledge
+# Difficulty: Unknown
+# Category: Auto
+# Topics: Auto, Done, 0 / 83 solved, 2 / 33 solved, 1 / 43 solved, 1 / 40 solved, 0 / 45 solved, 0 / 43 solved, 0 / 22 solved, 0 / 27 solved, 0 / 6 solved, 0 / 2 solved, 0 / 4 solved, 0 / 0 solved, 0 / 40 solved, 0 / 5 solved, QUESTS, Resets in 23h 22m, 60 / 330 XP · 100 / 550 coins, 0/1, NEW, SOON, 4,695
+# Source: https://whitebox.ac/knowledge
+# Pushed by Whitebox→GitHub Extension
+# Paste your solution here
