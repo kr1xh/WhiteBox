51e2ce23c259b777cf0592af9b4e3939c6c56ff3
Krish <krishchandpurkar20@gmail.com>
2026-06-03 15:46:46 +0530

    kr1xh: Time: N/A, Space: N/A - Whitebox→GitHub

--- kr1xh/kr1xh.py
+++ kr1xh/kr1xh.py
@@ -1,7 +1,7 @@
 # kr1xh
 # Difficulty: Easy 1
-# Category: BasicsData Structures
-# Topics: BasicsData Structures, Basics, Data Structures, Auto, Claim, Mark solved, POWER LEVEL, XP, RANK, SOLVED, ACCURACY, BEST STREAK, QUIZ SOLVED, QUIZ ACCURACY, PROB SOLVED, char III → char MAX, Jun, Wed, RANK UP, From, To, Jan, Feb, Mar, Apr, May, Jul, Aug, Sep, Oct, Nov, Dec, M  W  F, QUESTS, Resets in 22h 43m, 0 / 225 XP · 0 / 400 coins, 0/5, 0/1, NEW, SOON, 4,695, char III RANK, char III, char MAX, From char II  To char III, char II, +60 today
+# Category: Do:
+# Topics: Do:, Avoid:, Claimed today, BasicsData Structures, Basics, Data Structures, Auto, Claim, Mark solved, POWER LEVEL, XP, RANK, SOLVED, ACCURACY, BEST STREAK, QUIZ SOLVED, QUIZ ACCURACY, PROB SOLVED, char III → char MAX, Mixed, Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec, M  W  F, QUESTS, Resets in 22h 43m, 0 / 225 XP · 0 / 400 coins, 0/5, 0/1, NEW, SOON, 4,695, char III RANK, char III, char MAX, +60 today
 # Source: https://whitebox.ac/profile/kr1xh
 # Pushed by Whitebox→GitHub Extension
 # Paste your solution here
