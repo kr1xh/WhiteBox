c06b7b7dfc4e60b533c1977c127c3c2d771f5bc1
Krish <krishchandpurkar20@gmail.com>
2026-06-03 15:19:10 +0530

    interview-competitive-programming: Time: N/A, Space: N/A - Whitebox→GitHub

---
+++ interview-competitive-programming/interview-competitive-programming.py
@@ -0,0 +1,7 @@
+# Interview & Competitive Programming
+# Difficulty: Unknown
+# Category: Auto
+# Topics: Auto, Claim, Mark solved, Folders, QUESTS, Resets in 23h 10m, 0 / 225 XP · 0 / 400 coins, 0/5, 0/1, NEW, SOON, 4,695
+# Source: https://whitebox.ac/library/competitive-programming
+# Pushed by Whitebox→GitHub Extension
+# Paste your solution here
