f18c9004606b1abe40ea02456eb33ded805e3070
Krish <krishchandpurkar20@gmail.com>
2026-06-03 15:08:38 +0530

    job-board: Time: N/A, Space: N/A - Whitebox→GitHub

---
+++ job-board/job-board.py
@@ -0,0 +1,7 @@
+# Job Board
+# Difficulty: Unknown
+# Category: All
+# Topics: All, New Grad, Internships, Zapply, Simplify, Any time 1h 6h 24h 3d 7d 30d, Any time, 1h, 6h, 24h, 3d, 7d, 30d, Auto, Done, Location, QUESTS, Resets in 23h 21m, 60 / 330 XP · 100 / 550 coins, 0/1, NEW, SOON, 4,695, Closed
+# Source: https://whitebox.ac/jobs
+# Pushed by Whitebox→GitHub Extension
+# Paste your solution here
