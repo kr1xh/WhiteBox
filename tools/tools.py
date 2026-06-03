86d00df63420e9e926d526fb9efd96581945dd7e
Krish <krishchandpurkar20@gmail.com>
2026-06-03 15:20:54 +0530

    tools: Time: N/A, Space: N/A - Whitebox→GitHub

---
+++ tools/tools.py
@@ -0,0 +1,7 @@
+# Tools
+# Difficulty: Unknown
+# Category: Tags
+# Topics: Tags, atcoder, codeforces, contests, visualization, utility, codeforcescontests, All types linktool, All types, link, tool, All tags, Priority only, Auto, Claim, Mark solved, Priority, QUESTS, Resets in 23h 09m, 0 / 225 XP · 0 / 400 coins, 0/5, 0/1, NEW, SOON, 4,695
+# Source: https://whitebox.ac/library/cp-tools
+# Pushed by Whitebox→GitHub Extension
+# Paste your solution here
