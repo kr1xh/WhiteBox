3769a6c07d90e3c9e74829b51e9b91ce3b47b655
KRISH <krish.chandpurkar.bca@ghrstu.edu.in>
2026-08-10 16:31:38 +0530

    overriding-overloading: Time: 20 ms, Space: N/A - Whitebox→GitHub

--- overriding-overloading/overriding-overloading.cpp
+++ overriding-overloading/overriding-overloading.cpp
@@ -1,7 +1,8 @@
-// It's over for you. (if you can't do this)
+// Accepted
 // Difficulty: Unknown
-// Category: Basics  Millennium
-// Topics: Basics  Millennium, Basics, Auto, Mark solved, Done, Accepted, Acceptance, Next up, C++ 23, Whiteboard, QUESTS, Resets in 21h 58m, 60 / 270 XP · 100 / 450 coins, 0/1, 10s, 256 MB, Jan 1, 2024, 18.0ms
+// Category: Auto
+// Topics: Auto, Done, of C++ 23 solutions, Runtime, Memory, Language, Tests, C++ 23, Whiteboard, QUESTS, Resets in 21h 58m, 120 / 270 XP · 200 / 450 coins, 0/1, 20.0ms
+// Time: 20 ms
 // Source: https://whitebox.ac/problems/overriding-overloading
 // Pushed by Whitebox→GitHub Extension
 return x;
