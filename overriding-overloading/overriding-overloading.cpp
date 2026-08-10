6ec30028a11d6c68c166df38a0ad794aec02fd30
KRISH <krish.chandpurkar.bca@ghrstu.edu.in>
2026-08-10 16:28:35 +0530

    overriding-overloading: Time: N/A, Space: N/A - Whitebox→GitHub

---
+++ overriding-overloading/overriding-overloading.cpp
@@ -0,0 +1,22 @@
+// It's over for you. (if you can't do this)
+// Difficulty: Unknown
+// Category: Basics  Millennium
+// Topics: Basics  Millennium, Basics, Auto, Mark solved, Done, Accepted, Acceptance, Next up, C++ 23, Whiteboard, QUESTS, Resets in 22h 01m, 60 / 270 XP · 100 / 450 coins, 0/1, 10s, 256 MB, Jan 1, 2024, 18.0ms
+// Source: https://whitebox.ac/problems/overriding-overloading
+// Pushed by Whitebox→GitHub Extension
+return x;
+    }
+
+    virtual ~box() = default;
+};
+
+class whitebox : public box {
+public:
+    int foo() override {
+
+    virtual int foo(int x) {
+        return 0;
+    }
+public:
+    virtual int foo() {
+class box {
