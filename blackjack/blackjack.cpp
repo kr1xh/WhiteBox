0a6b46feab9b7c2df75db74a4eb88ecec3067f33
KRISH <krish.chandpurkar.bca@ghrstu.edu.in>
2026-08-15 16:05:16 +0530

    blackjack: Time: N/A, Space: N/A - Whitebox→GitHub

--- blackjack/blackjack.cpp
+++ blackjack/blackjack.cpp
@@ -1,7 +1,7 @@
 // Blackjack
 // Difficulty: Unknown
 // Category: Auto
-// Topics: Auto, Done, BLACKJACK, DEALER, YOU, QUESTS, Resets in 22h 25m, 120 / 270 XP · 200 / 450 coins, 0/1, NEW, White RoomNEW
+// Topics: Auto, Done, BLACKJACK, DEALER, YOU, QUESTS, Resets in 22h 24m, 120 / 270 XP · 200 / 450 coins, 0/1, NEW, White RoomNEW
 // Source: https://whitebox.ac/blackjack
 // Pushed by Whitebox→GitHub Extension
 # Paste your solution here
