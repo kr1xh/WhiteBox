77bbfd2d2a3ba3e13a732f85fa13de0ebf24c1d8
KRISH <krish.chandpurkar.bca@ghrstu.edu.in>
2026-08-15 18:21:12 +0530

    ratelimiter: Time: N/A, Space: N/A - Whitebox→GitHub

--- ratelimiter/ratelimiter.cpp
+++ ratelimiter/ratelimiter.cpp
@@ -1,7 +1,7 @@
-// Know your limits. (Easy version)
+// Daily Quests
 // Difficulty: Unknown
-// Category: BasicsData Structures +1 Akuna Capital
-// Topics: BasicsData Structures +1 Akuna Capital, Basics, Data Structures, +1, Auto, Done, Explanation, Accepted, Acceptance, Next up, Normal 3, C++ 23, Whiteboard, QUESTS, Resets in 20h 09m, 120 / 270 XP · 200 / 450 coins, 0/1, 10s, 256 MB, Jan 1, 2024, 1011.0ms
+// Category: Auto
+// Topics: Auto, Done, C++ 23, Whiteboard, Submitting..., QUESTS, Resets in 20h 08m, 120 / 270 XP · 200 / 450 coins, 0/1
 // Source: https://whitebox.ac/problems/ratelimiter
 // Pushed by Whitebox→GitHub Extension
 cout << "null\n";
